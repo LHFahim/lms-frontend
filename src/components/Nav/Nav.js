2817e63f4eba31fd50e2ae809a6c0a60052b69fe
LHFahim <fahimlutful@gmail.com>
2023-02-19 15:04:34 +0600

    added logout feature

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+
+const Nav = () => {
+  const { user, setUser } = useContext(AuthContext);
+
+  const loggedUser = user.user.user.firstName;
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setUser(null);
+    localStorage.removeItem('user');
+    navigate('/login');
+  };
+  return (
+    <main>
+      <div className="">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to={`/home`} className="text-lg">
+              Home
+            </Link>
+          </li>
+          <li>
+            <button onClick={handleLogout} className="text-lg">
+              Logout?
+            </button>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default Nav;
+{
+  /* <small>Logged in as {loggedUser},</small>  */
+}
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,27 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../contexts/AuthProvider/AuthProvider';
+import Nav from '../Nav/Nav';
 
 const Navbar = () => {
   return (
-    <header className="w-4/12 mx-auto bg-zinc-800">
-      <div className="flex justify-center mt-10 py-5 shadow-2xl">
-        <h1 className="text-3xl  font-black">
-          Online Library Management System
-        </h1>
-      </div>
-    </header>
+    <main className="grid grid-cols-[2fr,3fr] items-center">
+      <section className="">
+        <header className="w-4/12 mx-auto bg-zinc-800">
+          <div className="flex justify-center mt-10 py-5 shadow-2xl">
+            <h1 className="text-3xl  font-black">
+              Online Library Management System
+            </h1>
+          </div>
+        </header>
+      </section>
+      <section>
+        <Nav />
+      </section>
+    </main>
   );
 };
 
 export default Navbar;
+{
+}
